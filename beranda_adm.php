28a1fecbe6496a44f667da7f7c0c3c1345261429
rafli100101 <97889053+rafli100101@users.noreply.github.com>
2022-01-17 19:26:49 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Surat Dinas Pertanian</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/Index.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>Dinas Pertanian Asahan</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Arsip Dinas Pertanian Asahan.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data surat masuk dan keluar,<br>
+                            dan juga pengelolaan laporan surat yang sedang berjalan.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data surat masuk dan data surat keluar arsip.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
